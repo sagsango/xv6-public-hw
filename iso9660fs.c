ce031a1331911ab3e8532b7334631ce82a6ff171
sagsango <ssing214@uic.edu>
2023-11-24 00:04:36 -0600

    We have idea now how file/dirs are stored in CDROM
    This is just to know how cdrom stores the dirs and
    files.
    Now we have to figureout how to plug this in vfs.

--- iso9660fs.c
+++ iso9660fs.c
@@ -81,7 +81,7 @@ iso9660fsinit(char * const path, char * device)
 {
   /*
 
-      uint dev = ip->dev;
+    uint dev = ip->dev;
     uint addr = ip->inum;  
     // because we're not sure about the alignment of the record, always read two blocks
     static char buf[1024];
@@ -93,7 +93,7 @@ iso9660fsinit(char * const path, char * device)
     ip->size = entry->size;
     ip->addrs[0] = entry->extent*2048;
 
-*/
+  */
   cprintf("Mounting cdrom\n");
   begin_op();
   struct inode* mount_point = namei(path);
@@ -120,10 +120,52 @@ iso9660fsinit(char * const path, char * device)
     read_block_range(buf,addr/512,4);
     struct iso9660_dir_s *entry = buf;
 
+    /* XXX: cmd = mount iso9660 cdrom mnt */
     while (entry->length) {
       char namebuf[100]={0};
       memcpy(namebuf,&entry->filename.str[1],entry->filename.len);
       cprintf("Entry: %s\n",namebuf);
+      cprintf("length:%d\n"
+              "xa_length:%d\n"
+              "extent:%p\n"
+              "size:%d\n"
+              "file_flags:%d\n"
+              "file_unit_size:%d\n"
+              "interleave_gap:%d\n"
+              "volume_sequence_number:%d\n\n\n",
+              entry->length,
+              entry->xa_length,
+              entry->extent,
+              entry->size,
+              entry->file_flags,
+              entry->file_unit_size,
+              entry->interleave_gap,
+              entry->volume_sequence_number);
+      if(entry->file_flags == 0){
+          cprintf("File_content:");
+          char file_buf[2048] = {0};
+          int file_len = entry->size;
+          uint data_addr = entry->extent*2048;
+          read_block_range(file_buf, data_addr/512, 4);
+          file_buf[file_len] = 0;
+          cprintf("%s\n", file_buf);
+          cprintf("Len_was:%d\n", entry->size);
+      }
+      else if(entry->file_flags == 2) {
+          char file_buf[2048] = {0};
+          uint data_addr = entry->extent*2048;
+          read_block_range(file_buf, data_addr/512, 4);
+          struct iso9660_dir_s *my_entry = file_buf;
+          uint index = entry->extent*2048;
+          while (my_entry->length) {
+              char my_namebuf[100]={0};
+              memcpy(my_namebuf,&my_entry->filename.str[1],my_entry->filename.len);
+              cprintf("             Inside_Entry[at_idx:%d]: %s\n",index, my_namebuf);
+              index += my_entry->length;
+              my_entry = (struct iso9660_dir_s*) ((char*)my_entry+my_entry->length);
+          }
+
+      }
       entry = (struct iso9660_dir_s*) ((char*)entry+entry->length);
     }
 
